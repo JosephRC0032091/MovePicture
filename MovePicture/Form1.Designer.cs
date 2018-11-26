25d1cdaad0386ea254bf84ccc5d14d3fdfbfd199
Joseph Bamber <rc0032091@student.riversidecollege.ac.uk>
2018-11-26 15:39:58 +0000

    Part way did border control

--- MovePicture/Form1.Designer.cs
+++ MovePicture/Form1.Designer.cs
@@ -75,6 +75,7 @@
             resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
@@ -118,6 +119,7 @@
             this.Controls.Add(this.Up);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- MovePicture/Form1.cs
+++ MovePicture/Form1.cs
@@ -19,12 +19,14 @@ namespace MovePicture
 
         private void Up_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Location.Y > 0)
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X -5, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X > 0)
+                pictureBox1.Location = new Point(pictureBox1.Location.X -5, pictureBox1.Location.Y);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -34,7 +36,8 @@ namespace MovePicture
 
         private void button4_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
         }
 
         private void button2_Click_1(object sender, EventArgs e)
@@ -56,5 +59,20 @@ namespace MovePicture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
         }
-    }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+
+        {
+
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        
+            
+            {
+                
+            }
+        
+   }
 }
